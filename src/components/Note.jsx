30e66563504fe8d05b5d0aa9b47e09e89d4439cb
Nader Boutros <nadernabil2006@hotmail.com>
2023-09-30 22:54:04 -0400

    first commit 09-30-23

---
+++ src/components/Note.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import DeleteTwoToneIcon from "@mui/icons-material/DeleteTwoTone";
+
+function Note(props) {
+  function handleClick() {
+    props.onDelete(props.id);
+  }
+
+  return (
+    <div className="note">
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
+      <button onClick={handleClick}>
+        <DeleteTwoToneIcon />
+      </button>
+    </div>
+  );
+}
+
+export default Note;
