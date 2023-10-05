ee94af12be4f94deca557c6eb2673de8d39b2e71
Nader Boutros <nadernabil2006@hotmail.com>
2023-10-05 13:15:44 -0400

    rename 10-05-23

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
+import axios from "axios";
 
 function App() {
   const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    async function fetchNotes() {
+      try {
+        const response = await axios.get("http://localhost:4000/"); // Replace with your server's URL
+
+        setNotes(response.data);
+        console.log(notes);
+      } catch (error) {
+        console.error("Error fetching notes:", error);
+      }
+    }
+
+    fetchNotes(); // Call the fetchNotes function when the component mounts
+  }, [notes]);
 
   function addNote(newNote) {
     setNotes((prevNotes) => {
@@ -25,11 +41,11 @@ function App() {
     <div>
       <Header />
       <CreateArea onAdd={addNote} />
-      {notes.map((noteItem, index) => {
+      {notes.map((noteItem) => {
         return (
           <Note
-            key={index}
-            id={index}
+            key={noteItem._id} // Use _id as the unique identifier
+            id={noteItem._id} // Pass _id as id prop
             title={noteItem.title}
             content={noteItem.content}
             onDelete={deleteNote}
--- server/routes/router.js
+++ server/routes/router.js
@@ -4,7 +4,7 @@ import { Note } from "../db/conn.js";
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  const notes = await Note.find({});
+  const notes = await Note.find({}).exec();
   console.log(notes);
 });
 
